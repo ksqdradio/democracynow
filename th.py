0f7214b468bf3627e0f87efb8ba0797adb2a816c
Dan Nowacki <dan@nowacki.org>
2022-01-10 18:45:59 -0800

    Testing Thom Hartmann

---
+++ th.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import xmltodict
+import urllib.request
+import dropbox
+import pathlib
+import shutil
+import datetime
+
+
+readfile = False
+usedropbox = False
+
+
+def read_dn_xml(filename):
+    with urllib.request.urlopen(filename) as fd:
+        return xmltodict.parse(fd.read())
+
+
+def get_token(filename):
+    with open(filename, "r") as f:
+        return f.readline().strip()
+
+
+xml = read_dn_xml("http://audioport.org/rss.php?series=Thom+Hartmann+Program+-+Best+Of+One+Hour&id=37687970af9b74eafdb03455c8f47372")
+print(xml["rss"]["channel"]["item"].keys())
+latest = xml["rss"]["channel"]["item"]
+url = latest["enclosure"]["@url"]
+file_name = url.split("/")[-1]
+clean_file = latest["title"]
+clean_file = clean_file.split(":")[0]
+clean_file = "".join(clean_file.split())
+clean_file = clean_file + "-" + datetime.datetime.now().strftime('%Y-%m-%d') + ".mp3"
+print(f"Latest URL: {url}")
+print(f"Latest DN filename: {file_name}")
+print(f"Clean TH filename: {clean_file}")
+
+if readfile:
+    with urllib.request.urlopen(url) as response, open(clean_file, "wb") as out_file:
+        shutil.copyfileobj(response, out_file)
+
+if usedropbox:
+    apitoken = get_token("dropbox.apitoken")
+    d = dropbox.Dropbox(apitoken)
+    with open(file_name, "rb") as f:
+        meta = d.files_upload(
+            f.read(), "/" + file_name
+        )  # , mode=dropbox.files.WriteMode("overwrite"))
+    print(meta)
